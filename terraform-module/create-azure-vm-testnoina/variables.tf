fd88cfebe13edc86cb9900007a5e9fd1448cef98
Jenkins <jenkins@local>
2025-09-09 03:36:53 +0000

    Move Terraform module: create-azure-vm-testnoina

---
+++ terraform-module/create-azure-vm-testnoina/variables.tf
@@ -0,0 +1,49 @@
+//variables.tf
+
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "vm_name" {
+  type        = string
+}
+
+variable "vm_size" {
+  type        = string
+}
+
+variable "admin_username" {
+  type        = string
+}
+
+variable "admin_password" {
+  type        = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnet_prefix" {
+  type = list(string)
+}
+
+variable "allocation_method" {
+  type        = string
+}
+
+variable "public_ip_allocation_method" {
+  type    = string
+}
+
+variable "private_ip_allocation_method" {
+  type    = string
+}
+
+variable "public_ip_sku" {
+  type    = string
+}
